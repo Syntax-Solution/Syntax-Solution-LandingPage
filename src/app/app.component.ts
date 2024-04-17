3d7921747764b463206a5623e2e500cd8b1f7750
Egbule Goodluck <30560623+goodebag@users.noreply.github.com>
2024-04-17 02:20:21 +0100

    Footer corrctected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from "./components/header/header.component";
 import { BodySectionComponent } from "./components/body-section/body-section.component";
 import { ProductSectionComponent } from './components/product-section/product-section.component';
 import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent,BodySectionComponent,ProductSectionComponent,AboutSectionComponent]
+    imports: [RouterOutlet, HeaderComponent,BodySectionComponent,ProductSectionComponent,AboutSectionComponent,AppFooterComponent]
 })
 export class AppComponent {
   title = 'syntaxproject';
