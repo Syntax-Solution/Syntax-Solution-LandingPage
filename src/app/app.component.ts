9cd568bbc9171dcd4e1035a4582806e52af54b7b
Egbule Goodluck <30560623+goodebag@users.noreply.github.com>
2024-05-30 04:35:05 +0100

    AppBoddysection added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-
+import { BodySectionComponent } from "./components/body-section/body-section.component";
+import { ProductSectionComponent } from './components/product-section/product-section.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent,BodySectionComponent,ProductSectionComponent]
 })
 export class AppComponent {
   title = 'syntaxproject';
